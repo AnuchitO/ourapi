e6610fd8304d283ec53dd191ae8cbfd069eceb7a
AnuchitO <anuchitp@thoughtworks.com>
2019-10-05 09:28:56 +0700

    add users endpoint

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo"
+)
 
 func main() {
-	fmt.Println("hello, krub")
+	e := echo.New()
+
+	e.GET("/users", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{"hello": "hi"})
+	})
+
+	log.Fatal(e.Start(":1323"))
 }
