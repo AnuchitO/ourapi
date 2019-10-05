4135d7954f1ca3c6076d8104db72361694bff6a3
AnuchitO <anuchitp@thoughtworks.com>
2019-10-05 16:18:17 +0700

    change say to return channel

--- concurrency/main.go
+++ concurrency/main.go
@@ -6,17 +6,21 @@ import (
 	"time"
 )
 
-func saying(word string, c chan string) {
-	for i := 0; i < 5; i++ {
-		c <- fmt.Sprintf("you said: %s : %d", word, i)
-		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
-	}
+func saying(word string) <-chan string { // receive-only
+	c := make(chan string)
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			c <- fmt.Sprintf("you said: %s : %d", word, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+
+	return c
 }
 
 func main() {
-	c := make(chan string)
-
-	go saying("book fair", c)
+	c := saying("book fair")
 
 	for i := 0; i < 5; i++ {
 		fmt.Println("You say:", <-c)
